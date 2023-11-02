1837cff5c088892ff67a8744495c5c38178dfb6f
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-11-02 21:32:29 +0100

    Changes card component

--- src/app/shared/components/search-card/search-card.component.ts
+++ src/app/shared/components/search-card/search-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Result } from '@core/models/rickAnfMorty';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './search-card.component.html',
   styleUrls: ['./search-card.component.scss']
 })
