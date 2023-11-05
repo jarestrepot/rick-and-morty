992f49425f1cf641707c617d74fe1792b6d9a96a
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-11-05 16:53:26 +0100

    Filler text

--- src/app/components/main/main-pages/main.component.ts
+++ src/app/components/main/main-pages/main.component.ts
@@ -17,10 +17,10 @@ export class MajorComponent implements OnInit {
   ngOnInit(): void {
     this.elemetsCard =
       {
-        text: "elit. Corrupti, est numquam nobis harum suscipit alias sapiente necessitatibus deserunt consectetur quasi quisquam dignissimos quidem reprehenderit, distinctio doloremque quibusdam qui iste beatae.",
+        text: "On this page you can find your favorite character from the first 4 seasons of Rick and Morty. In this first presentation you can find characters 20 by 20 if you scroll you can go searching or the search engine will find for you among the characters that are in the batch, if you need a search engine for all the characters you can go to the chapters section.",
         img: {
           computer: '../../../../assets/img_ricky/imagecompressor/page-home.jpg',
-          movil: '../../../../assets/img_ricky/imagecompressor/movil-fondo-section.jpg'
+          movil: '../../../../assets/img_ricky/imagecompressor/logo-dark.png'
         }
       };
   }
